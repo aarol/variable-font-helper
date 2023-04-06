3131add139f5d93f8f4cbcf152b24cabbad26684
aarol <aarol@iki.fi>
2023-04-06 15:25:16 +0300

    remove CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,21 @@
 import * as functions from "firebase-functions";
 import { filterVariableFonts, Metadata } from "./metadata";
-import Axios from "axios";
-
-import cors = require("cors");
-const corsHandler = cors({ origin: "*" })
+import axios from "axios";
 
 export const getMetadata = functions.https.onRequest((async (request, response) => {
-  corsHandler(request, response, async () => {
-    response.set("Access-Control-Allow-Origin", "*") 
-    functions.logger.info("Getting metadata from Google fonts");
-    const url = "https://fonts.google.com/metadata/fonts";
-    
-    await Axios.get(url, { headers: {"User-Agent": "variable-font-helper.web.app"}})
-      .then((res) => {
-        const fonts = filterVariableFonts(res.data as Metadata)
-        // cache for 1 day
-        // on client AND cdn, so response is cached even if function is redeployed
-        response.set("Cache-Control", "public, max-age=86400, s-maxage=86400");
-        response.send(fonts)
-      })
-      .catch((err) => {
-        functions.logger.error("failed to get metadata:", err.toString());
-        response.sendStatus(500);
-      })
-  });
+  functions.logger.info("Getting metadata from Google fonts");
+  const url = "https://fonts.google.com/metadata/fonts";
+
+  await axios.get(url, { headers: { "User-Agent": "variable-font-helper.web.app" } })
+    .then((res) => {
+      const fonts = filterVariableFonts(res.data as Metadata)
+      // cache for 1 day
+      // on client AND CDN, so response is cached even if function is redeployed
+      response.set("Cache-Control", "public, max-age=86400, s-maxage=86400");
+      response.send(fonts)
+    })
+    .catch((err) => {
+      functions.logger.error("failed to get metadata:", err.toString());
+      response.sendStatus(500);
+    })
 }));
